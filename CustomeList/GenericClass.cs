ed74798c85efa62303f4d3c3033e21ac9d8dbb9d
Beng Cha <bengcha04@gmail.com>
2016-12-13 18:12:44 -0600

    upload new files

---
+++ CustomeList/GenericClass.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeList
+{
+    public class GenericClass<T> : IEnumerable<T>
+
+    {
+        int i;
+        bool remove;
+        T[] CustomArrayList;
+       
+        public string toString;
+
+        public GenericClass()
+        {
+            CustomArrayList = new T[0];
+            remove = true;
+
+        }
+
+
+        public void AddingObjectToList(T inputNewItem)
+        {
+            T[] addToColorList = new T[CustomArrayList.Length + 1];
+            for (i = 0; i < CustomArrayList.Length; i++)
+            {
+                addToColorList[i] = CustomArrayList[i];
+            }
+            addToColorList[CustomArrayList.Length] = inputNewItem;
+            CustomArrayList = addToColorList;
+        }
+
+        public void RemoveObjectFromList(T removeInputItem)
+        {
+            T[] removingFromArrayList = new T[CustomArrayList.Length - 1];
+            for (i = 0; i <= removingFromArrayList.Length; i++)
+            {
+                if (remove)
+                {
+                    if (CustomArrayList[i].Equals(removeInputItem))
+                    {
+                        remove = false;
+                    }
+                    else
+                    {
+                        removingFromArrayList[i] = CustomArrayList[i];
+                    }
+                }
+
+                else
+                {
+                    removingFromArrayList[i - 1] = CustomArrayList[i];
+                }
+
+            }
+            CustomArrayList = removingFromArrayList;
+        }
+
+
+        public void RemovingObject(T removeInputItem)
+        {
+            for (i = 0; i < CustomArrayList.Length; i++)
+            {
+                if (CustomArrayList[i].Equals(removeInputItem))
+                {
+                    RemoveObjectFromList(removeInputItem);
+                }
+            }
+
+
+        }
+
+        public void DisplayObjectInColorList()
+        {
+            foreach (T inputNewItem in CustomArrayList)
+            {
+                Console.WriteLine(inputNewItem);
+            }
+        }
+
+        public void DisplayerObjectInClotheList()
+        {
+            foreach (T inputNewItem1 in CustomArrayList)
+            {
+                Console.WriteLine(inputNewItem1);
+            }
+        }
+
+
+        public string ArrayListToString()
+        {
+            toString = "Object String:";
+            for (i = 0; i < CustomArrayList.Length; i++)
+            {
+                toString = toString + " " + CustomArrayList[i];
+            }
+
+            return toString;
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            for (i = 0; i < CustomArrayList.Length; i++)
+            {
+                yield return CustomArrayList[i];
+               
+            }
+            
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+
+
+}
+
+
---
+++ CustomeList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace CustomeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GenericClass<string> genericColorList = new GenericClass<string>();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Adding new Object to Color List");
+            Console.ResetColor();
+            genericColorList.AddingObjectToList("Red");
+            genericColorList.AddingObjectToList("Blue");
+            genericColorList.AddingObjectToList("White");
+            genericColorList.AddingObjectToList("Yellow");
+            genericColorList.AddingObjectToList("Green");
+            genericColorList.AddingObjectToList("Pink");
+            genericColorList.AddingObjectToList("Black \n");
+            genericColorList.DisplayObjectInColorList();
+
+            GenericClass<string> genericClotheList = new GenericClass<string>();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Adding new Object to Clothe List");
+            Console.ResetColor();
+            genericClotheList.AddingObjectToList("T-Shirt");
+            genericClotheList.AddingObjectToList("Jacket");
+            genericClotheList.AddingObjectToList("Hoodie");
+            genericClotheList.AddingObjectToList("Jean");
+            genericClotheList.AddingObjectToList("Sweater");
+            genericClotheList.AddingObjectToList("Boot");
+            genericClotheList.AddingObjectToList("Shoe");
+            genericClotheList.DisplayerObjectInClotheList();
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Removing a Object(Blue) from Color List");
+            Console.ResetColor();
+            genericColorList.RemoveObjectFromList("Blue");
+            genericColorList.DisplayObjectInColorList();
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nFrom Clothe List to String");
+            Console.ResetColor();
+            string toClotheString = genericClotheList.ArrayListToString();
+            string toColorString = genericColorList.ArrayListToString();
+            Console.WriteLine(toClotheString+ "\n" + toColorString);
+            Console.ReadLine();
+
+            GenericClass<string> colorClothe = new GenericClass<string>();
+            foreach (string CC in colorClothe)
+            {
+                Console.WriteLine(CC);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
